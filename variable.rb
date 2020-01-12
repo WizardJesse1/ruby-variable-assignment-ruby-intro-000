4600e402b30a75a6d7d7734b8c0591e328e8c9a0
Jesse Woolsey <jesse232@live.com>
2020-01-12 12:33:44 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,6 +1,8 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+ 
+ string_greeting = "Hello World"
+ 
 def greeting
-  string_greeting = "Hello World"
   puts string_greeting
 end
